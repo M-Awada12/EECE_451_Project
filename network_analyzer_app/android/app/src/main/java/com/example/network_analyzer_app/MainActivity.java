19501e091a7e3db92610552f6f729ee78a364341
M-Awada <m.awada1212@gmail.com>
2024-03-30 02:28:44 +0200

    get info in real time

--- network_analyzer_app/android/app/src/main/java/com/example/network_analyzer_app/MainActivity.java
+++ network_analyzer_app/android/app/src/main/java/com/example/network_analyzer_app/MainActivity.java
@@ -1,54 +1,196 @@
-package com.example.network_analyzer_app;
+ package com.example.network_analyzer_app;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.CellInfoNr;
+import android.telephony.CellInfoWcdma;
+import android.telephony.CellSignalStrengthGsm;
+import android.telephony.CellSignalStrengthLte;
+import android.telephony.CellSignalStrengthNr;
+import android.telephony.CellSignalStrengthWcdma;
+import android.telephony.TelephonyManager;
+import android.telephony.ServiceState;
+import android.util.Log;
+import android.telephony.SignalStrength;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import android.content.ContextWrapper;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.BatteryManager;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
-import android.os.Bundle;
 
 public class MainActivity extends FlutterActivity {
-    private static final String CHANNEL = "samples.flutter.dev/battery";
+    private static final String CHANNEL_NAME = "telephony_channel";
+    private TelephonyManager telephonyManager;
 
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
-        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
-                .setMethodCallHandler(
-                        (call, result) -> {
-                            // This method is invoked on the main thread.
-                            if (call.method.equals("getBatteryLevel")) {
-                                int batteryLevel = getBatteryLevel();
-
-                                if (batteryLevel != -1) {
-                                    result.success(batteryLevel);
-                                } else {
-                                    result.error("UNAVAILABLE", "Battery level not available.", null);
-                                }
-                            } else {
-                                result.notImplemented();
-                            }
-                        }
-                );
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL_NAME).setMethodCallHandler(
+                (call, result) -> {
+                    if (call.method.equals("getTelephonyInfo")) {
+                        Map<String, String> telephonyInfo = getTelephonyInfo();
+                        result.success(telephonyInfo);
+                    } else {
+                        result.notImplemented();
+                    }
+                }
+        );
     }
 
-    private int getBatteryLevel() {
-        int batteryLevel = -1;
-        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
-            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
-        } else {
-            Intent intent = new ContextWrapper(getApplicationContext()).
-                    registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-            batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
-                    intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        ServiceState serviceState = telephonyManager.getServiceState();
+    }
+
+    private Map<String, String> getTelephonyInfo() {
+        Map<String, String> telephonyInfo = new HashMap<>();
+        // Operator
+        telephonyInfo.put("operator", telephonyManager.getNetworkOperatorName());
+        // Signal Power
+        telephonyInfo.put("signalPower", getSignalStrength());
+        // SINR/SNR (Not available in all devices)
+        telephonyInfo.put("sinr", getSNR());
+        // Network Type
+        int networkType = telephonyManager.getNetworkType();
+        String networkTypeString;
+        switch (networkType) {
+            case TelephonyManager.NETWORK_TYPE_GPRS:
+            case TelephonyManager.NETWORK_TYPE_EDGE:
+            case TelephonyManager.NETWORK_TYPE_CDMA:
+            case TelephonyManager.NETWORK_TYPE_1xRTT:
+            case TelephonyManager.NETWORK_TYPE_IDEN:
+                networkTypeString = "2G";
+                break;
+            case TelephonyManager.NETWORK_TYPE_UMTS:
+            case TelephonyManager.NETWORK_TYPE_EVDO_0:
+            case TelephonyManager.NETWORK_TYPE_EVDO_A:
+            case TelephonyManager.NETWORK_TYPE_HSDPA:
+            case TelephonyManager.NETWORK_TYPE_HSUPA:
+            case TelephonyManager.NETWORK_TYPE_HSPA:
+            case TelephonyManager.NETWORK_TYPE_EVDO_B:
+            case TelephonyManager.NETWORK_TYPE_EHRPD:
+            case TelephonyManager.NETWORK_TYPE_HSPAP:
+                networkTypeString = "3G";
+                break;
+            case TelephonyManager.NETWORK_TYPE_LTE:
+                networkTypeString = "4G";
+                break;
+            default:
+                networkTypeString = "Unknown";
         }
-
-        return batteryLevel;
+        telephonyInfo.put("networkType", networkTypeString);
+        // Frequency Band
+        telephonyInfo.put("frequencyBand", getFrequencyBand());
+        // Cell ID
+        telephonyInfo.put("cellId", getCellInfo());
+        // Time Stamp
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy hh:mm a", Locale.getDefault());
+        telephonyInfo.put("timeStamp", sdf.format(new Date()));
+        return telephonyInfo;
     }
+
+    private String getSignalStrength() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            CellInfo cellInfo = getCellInfoObject();
+            if (cellInfo instanceof CellInfoLte) {
+                CellSignalStrengthLte cellSignalStrength = ((CellInfoLte) cellInfo).getCellSignalStrength();
+                return String.valueOf(cellSignalStrength.getDbm());
+            } else {
+                return "N/A";
+            }
+        } else {
+            return "N/A";
+        }
+    }
+
+    private String getSNR() {
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
+        CellInfo cellInfo = getCellInfoObject();
+        if (cellInfo instanceof CellInfoLte) {
+            CellSignalStrengthLte cellSignalStrength = ((CellInfoLte) cellInfo).getCellSignalStrength();
+
+            int rssi = (int) (10 * Math.log10(cellSignalStrength.getRssi()));
+
+            int rsrp = cellSignalStrength.getRsrp();
+
+            int snr = -rsrp - rssi;
+
+            return String.valueOf(snr);
+        } else {
+            return "N/A";
+        }
+    } else {
+        return "N/A";
+    }
+}
+
+    private CellInfo getCellInfoObject() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            List<CellInfo> cellInfoList = telephonyManager.getAllCellInfo();
+            if (cellInfoList != null) {
+                for (CellInfo cellInfo : cellInfoList) {
+                    if (cellInfo.isRegistered()) {
+                        return cellInfo;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private String getCellInfo() {
+    CellInfo cellInfo = getCellInfoObject();
+    if (cellInfo != null) {
+        if (cellInfo instanceof CellInfoGsm) {
+            CellInfoGsm cellInfoGsm = (CellInfoGsm) cellInfo;
+            return cellInfoGsm.getCellIdentity().getMcc() + "-" + cellInfoGsm.getCellIdentity().getCid();
+        } else if (cellInfo instanceof CellInfoLte) {
+            CellInfoLte cellInfoLte = (CellInfoLte) cellInfo;
+            return cellInfoLte.getCellIdentity().getMcc() + "-" + cellInfoLte.getCellIdentity().getCi();
+        } else if (cellInfo instanceof CellInfoWcdma) {
+            CellInfoWcdma cellInfoWcdma = (CellInfoWcdma) cellInfo;
+            return cellInfoWcdma.getCellIdentity().getMcc() + "-" + cellInfoWcdma.getCellIdentity().getCid();
+        }
+    }
+    return "";
+    }
+
+
+    private String getFrequencyBand() {
+    List<CellInfo> cellInfoList = telephonyManager.getAllCellInfo();
+    String frequencyBand = "";
+    for (CellInfo cellInfo : cellInfoList) {
+        if (cellInfo instanceof CellInfoGsm) {
+            frequencyBand = String.valueOf(((CellInfoGsm) cellInfo).getCellIdentity().getArfcn());
+            break;
+        } else if (cellInfo instanceof CellInfoWcdma) {
+            frequencyBand = String.valueOf(((CellInfoWcdma) cellInfo).getCellIdentity().getUarfcn());
+            break;
+        } else if (cellInfo instanceof CellInfoLte) {
+            frequencyBand = String.valueOf(((CellInfoLte) cellInfo).getCellIdentity().getEarfcn());
+            break;
+        }
+    }
+    return frequencyBand;
+}
+
 }
--- network_analyzer_app/lib/main.dart
+++ network_analyzer_app/lib/main.dart
@@ -1,68 +1,153 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static const platform = const MethodChannel('telephony_channel');
+
+  String operator = '';
+  String signalPower = '';
+  String sinr = '';
+  String networkType = '';
+  String frequencyBand = '';
+  String cellId = '';
+  String timeStamp = '';
+  String error = '';
+
+  late Timer _timer;
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  static const platform = MethodChannel('samples.flutter.dev/battery');
-  String _batteryLevel = 'Unknown battery level.';
-
-  Future<void> _getBatteryLevel() async {
-    String batteryLevel;
-    try {
-      final result = await platform.invokeMethod<int>('getBatteryLevel');
-      batteryLevel = 'Battery level at $result % .';
-    } on PlatformException catch (e) {
-      batteryLevel = "Failed to get battery level: '${e.message}'.";
-    }
-
-    setState(() {
-      _batteryLevel = batteryLevel;
+  void initState() {
+    super.initState();
+    _requestLocationPermission();
+    _timer = Timer.periodic(Duration(seconds: 5), (timer) {
+      _getTelephonyInfo();
     });
   }
 
   @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  Future<void> _requestLocationPermission() async {
+    if (await Permission.location.request().isGranted &&
+        await Permission.phone.request().isGranted) {
+      _getTelephonyInfo();
+    } else {
+      setState(() {
+        error = 'Location permission is not granted.';
+      });
+    }
+  }
+
+  Future<void> _getTelephonyInfo() async {
+    try {
+      final Map<dynamic, dynamic> result =
+          await platform.invokeMethod('getTelephonyInfo');
+      setState(() {
+        operator = result['operator'];
+        signalPower = result['signalPower'];
+        sinr = result['sinr'];
+        networkType = result['networkType'];
+        frequencyBand = result['frequencyBand'];
+        cellId = result['cellId'];
+        timeStamp = result['timeStamp'];
+        error = '';
+      });
+
+      print('Operator: $operator');
+      print('Signal Power: $signalPower dBm');
+      print('SINR/SNR: $sinr dB');
+      print('Network Type: $networkType');
+      print('Frequency Band: $frequencyBand');
+      print('Cell ID: $cellId');
+      print('Time Stamp: $timeStamp');
+    } on PlatformException catch (e) {
+      setState(() {
+        error = "Failed to get telephony info: '${e.message}'.";
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-              onPressed: _getBatteryLevel,
-              child: const Text('Get Battery Level'),
-            ),
-            Text(_batteryLevel),
-          ],
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        fontFamily: 'Roboto',
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Network Analyzer'),
         ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                if (error.isNotEmpty)
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20.0),
+                    child: Text(
+                      error,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                InfoTile(label: 'Operator', value: operator),
+                InfoTile(label: 'Signal Power', value: '$signalPower dBm'),
+                InfoTile(label: 'SINR/SNR', value: '$sinr dB'),
+                InfoTile(label: 'Network Type', value: networkType),
+                InfoTile(label: 'Frequency Band', value: '$frequencyBand MHz'),
+                InfoTile(label: 'Cell ID', value: cellId),
+                InfoTile(label: 'Time Stamp', value: timeStamp),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InfoTile extends StatelessWidget {
+  final String label;
+  final String value;
+
+  const InfoTile({
+    Key? key,
+    required this.label,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        label,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      subtitle: Text(
+        value,
+        style: TextStyle(fontSize: 16),
       ),
     );
   }
